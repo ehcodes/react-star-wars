b8b6ee1cdff532215a1a6c4884334021d4578065
ehcodes <engrid.hernandez@gmail.com>
2023-10-06 07:54:18 -0400

    returned the Starchip cards

--- src/components/Starships.jsx
+++ src/components/Starships.jsx
@@ -1,21 +1,30 @@
 import Starship from "./Starship.jsx";
-export default function Starships({ starships }) {
-  const loaded = (ships) => {
-    {
-      ships.map((ship, index) => {
-        return <Starship ship={ship} key={index} />;
-      });
-    }
-  };
 
-  const loading = () => {
+export default function Starships({ ships }) {
+  const loaded = () => {
+    console.log("loaded");
     return (
       <>
-        <h1>Starships</h1>
-        <p>Please wait while the starships load.</p>
+        {ships.map((swShip, index) => {
+          return (
+            <>
+              <Starship ship={swShip} key={index} />
+            </>
+          );
+        })}
       </>
     );
   };
 
-  return starships ? loaded(starships) : loading;
+  const loading = () => {
+    console.log("loading");
+    return (
+      <div>
+        <h1>Starships</h1>
+        <p>Please wait while the starships load.</p>
+      </div>
+    );
+  };
+
+  return ships ? loaded() : loading();
 }
